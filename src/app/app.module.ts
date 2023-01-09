ef1e9a5b92cd97a7a6498f7f69799588aeb107fc
rifaca11 <charifah1112@gmail.com>
2023-01-09 17:07:30 +0100

    add two components Ajout/List

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
 import { FirstComponent } from './first/first.component';
+import { AjoutComponent } from './ajout/ajout.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponent
+    FirstComponent,
+    AjoutComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
