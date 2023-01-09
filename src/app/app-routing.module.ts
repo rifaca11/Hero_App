93d2e7dffdd46f5c2bdb934053dfa4efcbcd1278
rifaca11 <charifah1112@gmail.com>
2023-01-09 18:12:32 +0100

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AjoutComponent} from "./ajout/ajout.component";
+import {ListComponent} from "./list/list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'ajout', component:AjoutComponent},
+  {path: 'list', component:ListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
-import { AjoutComponent } from './ajout/ajout.component';
-import { ListComponent } from './list/list.component';
+import {AjoutComponent} from "./ajout/ajout.component";
+import {ListComponent} from "./list/list.component";
+import {HeaderComponent} from "./header/header.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     AjoutComponent,
-    ListComponent
+    ListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
